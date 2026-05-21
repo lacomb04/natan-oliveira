0f8324e7b5dd8aa021848ea2965c8b76e144f893
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2026-05-21 03:17:02 +0000

    Colocando site pt-BR

--- natan-oliveira/app/layout.tsx
+++ natan-oliveira/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${nunito.variable} h-full antialiased`}>
+    <html lang="pt-BR" className={`${nunito.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
